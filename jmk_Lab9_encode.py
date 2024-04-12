04705544669d15531053f7dc4c8b590df90c120d
Realin Damuth <damuthrealin@ufl.edu>
2024-04-12 14:06:22 -0400

    complete code for jen via rea

--- jmk_Lab9_encode.py
+++ jmk_Lab9_encode.py
@@ -1,2 +1,40 @@
 #realins comment for testing
 
+def encode(password):
+    encoded = ""
+    for each_char in password:
+        each_char = int(each_char)
+        each_char += 3
+        encoded += str(each_char)
+
+    return encoded
+
+# realin = decode
+def decode(password):
+    decoded = ""
+    for each_char in password:
+        each_char = int(each_char)
+        each_char -= 3
+        decoded += str(each_char)
+    return decoded
+
+
+menu = print("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit""")
+
+choice = (input("Please enter an option: "))
+
+
+while choice == "1" or choice == "2" or choice == "3":
+    if choice == "2":
+        password_dec = (input("Insert the password to decode: "))
+        print(decode(password_dec))
+        choice = (input("Please enter an option: "))
+    elif choice == "1":
+        password_enc = (input("Insert the password to encode: "))
+        print(encode(password_enc))
+    elif choice == "3":
+        break
