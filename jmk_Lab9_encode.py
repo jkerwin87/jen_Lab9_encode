b39f06c16140092c349d20a69da2d84f55952e32
Realin Damuth <damuthrealin@ufl.edu>
2024-04-12 14:38:54 -0400

    realins commit

--- jmk_Lab9_encode.py
+++ jmk_Lab9_encode.py
@@ -9,3 +9,33 @@ def encode(password):
         encoded += str(each_char)
 
     return encoded
+def decode(password):
+    decoded = ""
+    for each_char in password:
+        each_char = int(each_char)
+        each_char -= 3
+        decoded += str(each_char)
+    return decoded
+
+
+menu = print("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit""")
+
+choice = (input("Please enter an option: "))
+
+
+while choice == "1" or choice == "2" or choice == "3":
+    if choice == "2":
+        y = (decode(x))
+        print(f"The encoded password is {x}, and the original password is {y}")
+        choice = (input("Please enter an option: "))
+    elif choice == "1":
+        password_enc = (input("Insert the password to encode: "))
+        x = (encode(password_enc))
+        print("Your password has been encoded and stored!")
+        choice = (input("Please enter an option: "))
+    elif choice == "3":
+        break
